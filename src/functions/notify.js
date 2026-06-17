16995ec851b10a624d25160ccd47fad0e2416ccc
Grazzi <gacascardi@outlook.com>
2026-06-17 06:31:00 -0300

    Atualizar o notify.js

--- src/functions/notify.js
+++ src/functions/notify.js
@@ -1,16 +1,15 @@
-// Arquivo notify.js
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
 
-app.http('notify', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
+app.http("notify", {
+    methods: ["GET"],
+    authLevel: "anonymous",
+
     handler: async (request, context) => {
 
         return {
-            status: 200,
             jsonBody: {
-                title: "Mangostin ❤️",
-                message: "Mais um dia amando você."
+                status: "ok",
+                message: "Mangostin funcionando ❤️"
             }
         };
 
