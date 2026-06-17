99624cc7d19873e82fae8a558e29a3b4221a174f
Grazzi <gacascardi@outlook.com>
2026-06-17 06:42:04 -0300

    Criar anniversary.js

---
+++ src/functions/anniversary.js
@@ -0,0 +1,29 @@
+const { app } = require("@azure/functions");
+
+app.http("anniversary", {
+    methods: ["GET"],
+    authLevel: "anonymous",
+
+    handler: async (request, context) => {
+
+        const namoroInicio = new Date("2026-03-07");
+        const hoje = new Date();
+
+        let meses =
+            (hoje.getFullYear() - namoroInicio.getFullYear()) * 12 +
+            (hoje.getMonth() - namoroInicio.getMonth());
+
+        const aniversario =
+            hoje.getDate() === namoroInicio.getDate();
+
+        return {
+            jsonBody: {
+                anniversary: aniversario,
+                months: meses,
+                message: aniversario
+                    ? `Hoje completamos ${meses} meses de namoro ❤️`
+                    : "Hoje não é aniversário mensal."
+            }
+        };
+    }
+});
