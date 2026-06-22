aa9a972861bd76748eec4619197e3d1687da08c5
Grazzi <gacascardi@outlook.com>
2026-06-22 18:58:54 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// index.js - Adaptado para Azure Flex Consumption
+const path = require('path');
 
-require("./src/functions/notify");
-require("./src/functions/anniversary");
-require("./src/functions/sendtest"); // Adicionando a nova rota de teste
+// Carrega as funções garantindo que o caminho absoluto seja exato dentro do pacote zipado
+require(path.join(__dirname, 'src', 'functions', 'notify.js'));
+require(path.join(__dirname, 'src', 'functions', 'anniversary.js'));
+require(path.join(__dirname, 'src', 'functions', 'sendtest.js'));
