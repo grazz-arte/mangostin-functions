7e72a0b24476c7179ddd7310561847ab572c5504
Grazzi <gacascardi@outlook.com>
2026-06-22 20:37:59 -0300

    Remove comments and clean up index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 // index.js - Adaptado para Azure Flex Consumption
-const path = require('path');
-
-// Carrega as funções garantindo que o caminho absoluto seja exato dentro do pacote zipado
-require(path.join(__dirname, 'src', 'functions', 'notify.js'));
-require(path.join(__dirname, 'src', 'functions', 'anniversary.js'));
-require(path.join(__dirname, 'src', 'functions', 'sendtest.js'));
