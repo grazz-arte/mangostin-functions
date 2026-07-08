ab66e03015840b08064ac65c1ac4906977143aba
Grazzi <gacascardi@outlook.com>
2026-07-07 23:12:55 -0300

    Atualizar o index.js scheduler

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ require("./src/functions/notify");
 require("./src/functions/anniversary");
 require("./src/functions/sendtest");
 require("./src/functions/registertoken");
+require("./src/functions/scheduler");
